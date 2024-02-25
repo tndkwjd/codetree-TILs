78b00b77142a99ced319532549113a742c4976a1
tndkwjd <149052717+tndkwjd@users.noreply.github.com>
2024-02-25 14:49:20 +0900

    Title: 삼항연산자, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 240225/삼항연산자/ternary-operator.java
+++ 240225/삼항연산자/ternary-operator.java
@@ -4,12 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int score = sc.nextInt();
-        int tmp; 
-        tmp = score == 100 ? 0 : 1;
-        if(tmp == 0){
-            System.out.println("pass");
-        }else{
-            System.out.println("failure");
-        }
+        System.out.println(score == 100? "pass" : "failure");
     }
 }
