87dccf6fd8ff8bde95bad7b5ca1f4bede8d75cdb
tndkwjd <149052717+tndkwjd@users.noreply.github.com>
2024-02-25 12:34:00 +0900

    Title: 합과 평균, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240225/합과 평균/sum-and-avg.java
+++ 240225/합과 평균/sum-and-avg.java
@@ -5,6 +5,6 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        System.out.printf("%d, %.1f", a + b, (double) (a + b) / 2);
+        System.out.printf("%d %.1f", a + b, (double) (a + b) / 2);
     }
 }
