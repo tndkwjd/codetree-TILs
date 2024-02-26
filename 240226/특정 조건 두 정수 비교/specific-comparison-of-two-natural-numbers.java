2a9485b11a4e08ffe2d9270f4478d2eef4a4634d
tndkwjd <149052717+tndkwjd@users.noreply.github.com>
2024-02-26 12:32:45 +0900

    Title: 특정 조건 두 정수 비교, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 240226/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
+++ 240226/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
@@ -5,8 +5,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        if(a < b || a == b || a != b){
-            System.out.println(1 + " " + 0);
+        if(a < b){
+            System.out.print(1 + " ");
+        }else{
+            System.out.print(0 + " ");
+        }
+
+        if(a == b){
+            System.out.print(1 + " ");
+        }else{
+            System.out.print(0 + " ");
         }
     }
 }
