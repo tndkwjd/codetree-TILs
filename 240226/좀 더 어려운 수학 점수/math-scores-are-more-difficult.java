7f7dd4be4a1defec990da7518d46686b3d5e3f01
tndkwjd <149052717+tndkwjd@users.noreply.github.com>
2024-02-26 14:37:00 +0900

    Title: 좀 더 어려운 수학 점수, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240226/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Am = sc.nextInt();
+        int Bm = sc.nextInt();
+        int Ae = sc.nextInt();
+        int Be = sc.nextInt();
+        if(Am >= Bm && Be >= Ae)
+            System.out.println("A");
+        else
+            System.out.println("B");
+    }
+}
