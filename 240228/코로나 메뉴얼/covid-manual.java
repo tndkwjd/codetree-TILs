2683dc3b95059ba52e28c67c68bfb04ea14c4e15
tndkwjd <149052717+tndkwjd@users.noreply.github.com>
2024-02-28 09:22:10 +0900

    Title: 코로나 메뉴얼, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 240228/코로나 메뉴얼/covid-manual.java
+++ 240228/코로나 메뉴얼/covid-manual.java
@@ -3,14 +3,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int cold = sc.nextInt();
-        if (cold >= 37)
-            System.out.println("A");
-        if(cold != 37)
-            System.out.println("B");
-        if(cold == 36)
-            System.out.println("C");
-        else
-            System.out.println("D");
+        char s1 = sc.next().charAt(0);
+        int c1 = sc.nextInt();
+        char s2 = sc.next().charAt(0);
+        int c2 = sc.nextInt(); 
+        char s3 = sc.next().charAt(0);
+        int c3 = sc.nextInt();
+        if(s1 == 'Y' && c1 >= 37){
+            if((s2 == 'Y'&& c2 >= 37) || s3 == 'Y' && c3 >= 37){
+                System.out.println("E");
+            }else{
+                System.out.println("N");
+            }
+        }
+        else{
+            if((s2 == 'Y' && c2 >= 37) && (s3 == 'Y' && c3 >= 37)){
+                System.out.println("E");
+            }else{
+                System.out.println("N");
+            }
+        }
     }
 }
