4c38a9493b0d45a870770fe0ef3d21cc2d95a143
tndkwjd <149052717+tndkwjd@users.noreply.github.com>
2024-03-12 21:06:21 +0900

    Title: 100점 도달하기, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 240312/100점 도달하기/reach-100-point.java
+++ 240312/100점 도달하기/reach-100-point.java
@@ -6,15 +6,15 @@ public class Main {
         int n = sc.nextInt();
         for(int i = n; i <= 100; i++){
             if(i >= 90){
-                System.out.print("A" + " ");
+                System.out.print("A ");
             }else if(i >= 80){
-                System.out.print("B" + " ");
+                System.out.print("B ");
             }else if(i >= 70){
-                System.out.print("C" + " ");
+                System.out.print("C ");
             }else if(i >= 60){
-                System.out.print("D" + " ");
+                System.out.print("D ");
             }else{
-                System.out.print("F" + " ");
+                System.out.print("F ");
             }
         }
     }
