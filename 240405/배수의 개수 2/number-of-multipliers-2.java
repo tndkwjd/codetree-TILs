2641bd6cae902cb1f3c26ceb46710bafe1d4ff14
tndkwjd <149052717+tndkwjd@users.noreply.github.com>
2024-04-05 21:17:19 +0900

    Title: 배수의 개수 2, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240405/배수의 개수 2/number-of-multipliers-2.java
+++ 240405/배수의 개수 2/number-of-multipliers-2.java
@@ -4,9 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int cnt = 0; 
-        for(int i = 1; i <= 10; i++){
-          if(i % 2 == 1){
-            cnt++;
+        for(int i = 0; i < 10; i++){
+            int num = sc.nextInt();    
+            if(num % 2 == 1){
+                cnt++;
           }
         }
         System.out.println(cnt);
