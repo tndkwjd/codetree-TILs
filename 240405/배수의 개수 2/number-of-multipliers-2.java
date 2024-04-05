0fcde7502854d038a1d421845b7864037ef2f901
tndkwjd <149052717+tndkwjd@users.noreply.github.com>
2024-04-05 21:23:54 +0900

    Title: 배수의 개수 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240405/배수의 개수 2/number-of-multipliers-2.java
+++ 240405/배수의 개수 2/number-of-multipliers-2.java
@@ -3,10 +3,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int cnt = 0; 
-        for(int i = 0; i < 10; i++){
-            int num = sc.nextInt();    
-            if(num % 2 == 1){
+        int a, cnt = 0; 
+        for(int i = 1; i <= 10; i++){  
+            if(a % 2 == 1){
                 cnt++;
           }
         }
